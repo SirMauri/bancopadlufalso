74b40d9b3699be7a5b1cf6d0b62016df497974ff
carlos <carlos@sirmauri.dev>
2021-04-07 06:39:24 -0500

    aoeu

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Sinkhole</title>
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link href='https://fonts.googleapis.com/css2?family=DotGothic16&display=swap' rel='stylesheet' />
+      </Head>
+
+      <main>
+        <h1 style={{ color: 'white', marginRight: '10px' }}>
+          Pagina FALSA de banco de padlu. Te vamos a hacker :)
+        </h1>
+      </main>
+    </div>
+  );
+}
